c41cfacc446c52bac1930660c94580425d9b16a5
Tushar Dhakate <tushardhakate6@gmail.com>
2026-03-08 17:58:32 +0530

    Time: 4 ms (19.45%), Space: 43.3 MB (50.14%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.java
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -1,14 +1,26 @@
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
-        StringBuilder s=new StringBuilder();
-        for(int i=0;i<nums.length;i++){
-            if(nums[i].charAt(i)=='0'){
-                s.append('1');
+        HashSet<String> s=new HashSet<>();
+        HashSet<String> processed=new HashSet<>();
+        for(String i:nums) s.add(i);
+        return func(nums,nums[0],s,processed);
+    }
+    String func(String nums[],String curr,HashSet<String> s,HashSet<String> processed){
+        if(processed.contains(curr)) return null;
+        processed.add(curr);
+        for(int i=0;i<curr.length();i++){
+            String newstring="";
+            if(curr.charAt(i)=='0'){
+                newstring=curr.substring(0,i)+"1"+curr.substring(i+1,curr.length());
             }
             else{
-                s.append('0');
+                newstring=curr.substring(0,i)+"0"+curr.substring(i+1,curr.length());
+
             }
+            if(!s.contains(newstring)) return newstring;
+            String returned=func(nums,newstring,s,processed);
+            if(returned!=null) return returned;
         }
-        return s.toString();
+        return null;
     }
 }
