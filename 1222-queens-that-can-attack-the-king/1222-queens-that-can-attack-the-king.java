1563fbbed0981edf9ec03fde5d0a371b37fd7c5c
Tushar Dhakate <tushardhakate6@gmail.com>
2026-04-04 23:24:31 +0530

    Time: 3 ms (21.19%), Space: 44.5 MB (36.02%) - LeetHub

--- 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.java
+++ 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.java
@@ -1,55 +1,43 @@
 class Solution {
     public List<List<Integer>> queensAttacktheKing(int[][] queens, int[] king) {
-
         List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < queens.length; i++) {
+            int curr[] = queens[i];
+            int newkingx = king[0] - curr[0];
+            int newkingy = king[1] - curr[1];
+            if (curr[0] == king[0] || curr[1] == king[1] || Math.abs(newkingx) == Math.abs(newkingy)) {
+                boolean found = false;
+                for (int j = 0; j < queens.length; j++) {
+                    int temp[] = queens[j];
+                    int newqueenx = temp[0] - curr[0];
+                    int newqueeny = temp[1] - curr[1];
+                    if ((curr[0] == temp[0] && curr[0] == king[0]) ||
+                            (curr[1] == temp[1] && curr[1] == king[1]) ||
+                            Math.abs(newqueenx) == Math.abs(newqueeny)) {
 
-        for(int i=0;i<queens.length;i++){
-
-            int[] curr = queens[i];
-
-            int newkingx = king[0]-curr[0];
-            int newkingy = king[1]-curr[1];
-
-            if(curr[0]==king[0] ||
-               curr[1]==king[1] ||
-               Math.abs(newkingx)==Math.abs(newkingy)){
-
-                boolean found=false;
-
-                for(int j=0;j<queens.length;j++){
-
-                    if(i==j) continue;
-
-                    int[] temp=queens[j];
-
-                    int newqueenx=temp[0]-curr[0];
-                    int newqueeny=temp[1]-curr[1];
-
-                    boolean sameLine =
-                        (curr[0]==king[0] && temp[0]==curr[0]) ||
-                        (curr[1]==king[1] && temp[1]==curr[1]) ||
-                        (Math.abs(newqueenx)==Math.abs(newqueeny));
-
-                    boolean sameDirection =
-                        Integer.signum(newqueenx)==Integer.signum(newkingx) &&
-                        Integer.signum(newqueeny)==Integer.signum(newkingy);
-
-                    boolean closer =
-                        Math.max(Math.abs(newqueenx), Math.abs(newqueeny)) <
-                        Math.max(Math.abs(newkingx), Math.abs(newkingy));
-
-                    if(sameLine && sameDirection && closer){
-                        found=true;
-                        break;
+                        if (((newkingx > 0 && newqueenx > 0 && newkingy > 0 && newqueeny > 0)
+                                || (newkingx > 0 && newqueenx > 0 && newkingy < 0 && newqueeny < 0)
+                                || (newkingx < 0 && newqueenx < 0 && newkingy > 0 && newqueeny > 0)
+                                || (newkingx < 0 && newqueenx < 0 && newkingy < 0 && newqueeny < 0)
+                                || (newkingx == 0 && newqueenx == 0 && newkingy > 0 && newqueeny > 0)
+                                || (newkingx == 0 && newqueenx == 0 && newkingy < 0 && newqueeny < 0)
+                                || (newkingx > 0 && newqueenx > 0 && newkingy == 0 && newqueeny == 0)
+                                || (newkingx < 0 && newqueenx < 0 && newkingy == 0 && newqueeny == 0))
+                                && Math.max(Math.abs(newqueenx),Math.abs(newqueeny)) < 
+                                Math.max(Math.abs(newkingx),Math.abs(newkingy))) {
+                            found = true;
+                            break;
+                        }
                     }
                 }
-
                 if(!found){
-                    ans.add(Arrays.asList(curr[0], curr[1]));
+                    List<Integer> l=new ArrayList<>();
+                    l.add(curr[0]);
+                    l.add(curr[1]);
+                    ans.add(l);
                 }
             }
         }
-
         return ans;
     }
 }
