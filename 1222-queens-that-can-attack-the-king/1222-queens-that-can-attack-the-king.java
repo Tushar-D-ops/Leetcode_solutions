e6ab49891b0114af86aaf2bb61759b8e356ebf8c
Tushar Dhakate <tushardhakate6@gmail.com>
2026-04-04 23:31:08 +0530

    Time: 1 ms (85.59%), Space: 44.6 MB (24.58%) - LeetHub

--- 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.java
+++ 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.java
@@ -1,41 +1,25 @@
 class Solution {
     public List<List<Integer>> queensAttacktheKing(int[][] queens, int[] king) {
-        List<List<Integer>> ans = new ArrayList<>();
-        for (int i = 0; i < queens.length; i++) {
-            int curr[] = queens[i];
-            int newkingx = king[0] - curr[0];
-            int newkingy = king[1] - curr[1];
-            if (curr[0] == king[0] || curr[1] == king[1] || Math.abs(newkingx) == Math.abs(newkingy)) {
-                boolean found = false;
-                for (int j = 0; j < queens.length; j++) {
-                    int temp[] = queens[j];
-                    int newqueenx = temp[0] - curr[0];
-                    int newqueeny = temp[1] - curr[1];
-                    if ((curr[0] == temp[0] && curr[0] == king[0]) ||
-                            (curr[1] == temp[1] && curr[1] == king[1]) ||
-                            Math.abs(newqueenx) == Math.abs(newqueeny)) {
-
-                        if (((newkingx > 0 && newqueenx > 0 && newkingy > 0 && newqueeny > 0)
-                                || (newkingx > 0 && newqueenx > 0 && newkingy < 0 && newqueeny < 0)
-                                || (newkingx < 0 && newqueenx < 0 && newkingy > 0 && newqueeny > 0)
-                                || (newkingx < 0 && newqueenx < 0 && newkingy < 0 && newqueeny < 0)
-                                || (newkingx == 0 && newqueenx == 0 && newkingy > 0 && newqueeny > 0)
-                                || (newkingx == 0 && newqueenx == 0 && newkingy < 0 && newqueeny < 0)
-                                || (newkingx > 0 && newqueenx > 0 && newkingy == 0 && newqueeny == 0)
-                                || (newkingx < 0 && newqueenx < 0 && newkingy == 0 && newqueeny == 0))
-                                && Math.max(Math.abs(newqueenx),Math.abs(newqueeny)) < 
-                                Math.max(Math.abs(newkingx),Math.abs(newkingy))) {
-                            found = true;
-                            break;
-                        }
-                    }
-                }
-                if(!found){
+        int [][] board=new int[8][8];
+        List<List<Integer>> ans=new ArrayList<>();
+        for(int curr[]:queens){
+            board[curr[0]][curr[1]]=1;
+        }
+        int dir[][]={{1,0},{-1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
+        for(int d[]:dir){
+            int kingx=king[0];
+            int kingy=king[1];
+            while(true){
+                if(kingx<0 || kingy<0 || kingx>7 || kingy>7) break;
+                if(board[kingx][kingy]==1){
                     List<Integer> l=new ArrayList<>();
-                    l.add(curr[0]);
-                    l.add(curr[1]);
+                    l.add(kingx);
+                    l.add(kingy);
                     ans.add(l);
+                    break;
                 }
+                kingx+=d[0];
+                kingy+=d[1];
             }
         }
         return ans;
